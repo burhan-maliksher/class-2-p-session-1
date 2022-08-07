7a20e2d444388ec25a5d7ef284a17ce4e4348ec6
Muhammad Burhan <66227807+burhan-maliksher@users.noreply.github.com>
2022-08-07 10:32:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          hello deployed on surge and yml file working with no error
         </p>
         <a
           className="App-link"
